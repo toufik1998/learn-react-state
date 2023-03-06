9746118240606d4d2a9413c4b4f7ea3457095021
toufik1998 <toufikshima98@gmail.com>
2023-03-06 13:32:24 +0100

    create a simple counter with state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [count, setCount] = React.useState(0)
+    
+    function add() {
+        // setCount(count + 1)
+        setCount(preveCount => preveCount + 1)
+    }
+    
+    function subtract() {
+        // setCount(count - 1)
+        setCount(preveCount => preveCount - 1)
+
+    }
+    
+    return (
+        <div className="counter">
+            <button className="counter--minus" onClick={subtract}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={add}>+</button>
+        </div>
+    )
 }
-
-export default App;
